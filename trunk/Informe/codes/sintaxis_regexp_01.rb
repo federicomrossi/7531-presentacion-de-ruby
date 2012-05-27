d7fc927ae46ee2eea0b88f1475d549c054012f94
nomevaagustar <nomevaagustar@545e6492-1596-d6f9-9553-0ca6dd65564b>
2012-05-27 21:32:05 +0000

    Se agregan ejemplos en sintaxis de strings y de regexpr

--- trunk/Informe/codes/sintaxis_regexp_01.rb
+++ trunk/Informe/codes/sintaxis_regexp_01.rb
@@ -1,4 +1,16 @@
-#Expresiones regulares
+#Ejemplo 1
+/abc/  =~ 'zxyabcdef'     # Returns TRUE
+/ABC/  =~ 'zxyabcdef'     # Returns FALSE
+/ABC/i =~ 'zxyabcdef'     # Returns TRUE
+
+#Ejemplo 2
+/^From:/ =~ "From: jack"    # Returns TRUE
+/^ To:/  =~ "To: jill"     # Returns FALSE
+
+/end$/   =~ "to the end"    # Returns TRUE
+/end$/   =~ "to an end!"    # Returns False
+
+#Ejemplo 3
 def chab(s) # contiene la cadena un hexadecimal (entre angulos "<>")
   (s =~ /<0[Xx][\dA-Fa-f]+>/) != nil
 end
---
+++ trunk/Informe/codes/sintaxis_strings_04.rb
@@ -0,0 +1,10 @@
+#Creando strings...
+%Q(\ttext)	# "<tab>text"
+%q(\ttext)	# "\text"
+
+%Q#--text--# # "--text--"
+
+#Array de strings
+arr = %w(cat dog whale tiger great dane)
+#["cat", "dog", "whale", "tiger", "great", "dane"]
+
